b7c6ada6387fd1b690a2e781960b3415b277bb00
JASmedley <jasmine.a.smedley@gmail.com>
2023-11-02 19:12:39 -0500

    adding a public folder

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 let express = require("express");
+app.use(express.static("public"))
+
 let PORT = 8001;
 
 let app = express();
@@ -7,12 +9,11 @@ app.use(express.json());
 let routes = require("./routes");
 app.use(routes);
 
-// app.get("/hello", function(req, res){
-//     console.log("In GET /hello");
-//     resizeBy.json("Hello there.")
-//   });
-
 app.listen(PORT, function(){
     console.log("Application started on port", PORT);
   });
 
+// app.get("/hello", function(req, res){
+//     console.log("In GET /hello");
+//     resizeBy.json("Hello there.")
+//   });
